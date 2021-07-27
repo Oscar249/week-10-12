1e719053fef770babad6a6e499a334944d7ef27f
Oscar249 <80767588+Oscar249@users.noreply.github.com>
2021-07-27 08:34:57 -0500

    Create chapter13ex02-OscarRomo.c

---
+++ chapter13ex02-OscarRomo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+char *White(char *string); /*searches the first non-white character*/
+
+int main()
+{
+	char string[] = "Enter the string here"; /*The string value*/ 
+
+	printf("the following character is the first non-white character: '%c'\n", *White(string)); /*print what we found in the first non-space character*/
+
+	return 0;
+}
+
+char *White(char *string) /*switch to function*/.
+{
+	while (*string != '\0') {
+		if (! isspace(*string))
+			return string; 
+		++string;
+	}
+
+	return string;
+}
+
+Translated with www.DeepL.com/Translator (free version)
